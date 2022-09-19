51a17c3ddabac7f0bcac5f6758e475fd42c9cafc
Sam Bell <me@sam-bell.com>
2022-09-19 14:11:22 +0000

    feat: set permissions script

---
+++ permissions.sh
@@ -0,0 +1,13 @@
+chown -R www-data storage/app
+chown -R www-data storage/framework
+chown -R www-data storage/logs
+chown -R www-data storage/cache
+chown -R www-data bootstrap/cache
+chown -R www-data .env
+
+chmod -R a+w storage/app
+chmod -R a+w storage/framework
+chmod -R a+w storage/logs
+chmod -R a+w storage/cache
+chmod -R a+w bootstrap/cache
+chmod -R a+w .env
