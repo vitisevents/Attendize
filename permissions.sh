10512f4ca780822c77aacca25b473edc433c68d0
Sam Bell <me@sam-bell.com>
2022-09-19 20:16:57 +0000

    chore: folder owner

--- permissions.sh
+++ permissions.sh
@@ -1,3 +1,4 @@
+chown -R www-data public/user_content
 chown -R www-data storage/app
 chown -R www-data storage/framework
 chown -R www-data storage/logs
@@ -5,6 +6,7 @@ chown -R www-data storage/cache
 chown -R www-data bootstrap/cache
 chown -R www-data .env
 
+chmod -R a+w public/user_content
 chmod -R a+w storage/app
 chmod -R a+w storage/framework
 chmod -R a+w storage/logs
